126ccc9b6732a59b44acb3c1735966bbaaf1fcf7
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-06 14:27:19 +0700

    нахождение max числа из 3

---
+++ аpp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace аpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Введите целое число a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите целое число b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите целое число c");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int max = a;
+
+            if (a > max)
+            {
+                max = a;
+            }
+
+            if (b > max)
+            {
+                max = b;
+            }
+
+            if (c > max)
+            {
+                max = c;
+            }
+
+            Console.WriteLine("max = " + max);
+        }
+    }
+}
