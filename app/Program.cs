2a2c807d704c19c892d8932da5da05db981f85ea
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-06 14:22:49 +0700

    нахождение max числа из 2

---
+++ app/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Введите целое число a");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите целое число b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("max = " + a);
+            }
+            else
+            {
+                Console.WriteLine("max = " + b);
+            }
+        }
+    }
+}
