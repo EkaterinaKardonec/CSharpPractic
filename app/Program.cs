87a99efe77d3cb5a6e0d26d397bce0325c971c96
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-06 14:32:52 +0700

    определение четности/нечестности числа

---
+++ app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("число является четным");
+            }
+            else
+            {
+                Console.WriteLine("число является нечетным");
+            }
+        }
+    }
+}
