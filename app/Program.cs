679b7a8239576bac42ccbbf17c065d145e76c934
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-09 22:06:45 +0700

    определение выходных дней (через switch)

--- app/Program.cs
+++ app/Program.cs
@@ -9,19 +9,24 @@ namespace app
             Console.Write("Введите номер дня недели ");
 
             var a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.Write("это будний день");
+                    break;
+                case 6:
+                case 7:
+                    Console.Write("это выходной день");
+                    break;
+                default:
+                    Console.Write("Это не день недели");
+                    break;
+            }
 
-            if (a>=1 && a<=5)
-            {
-                Console.Write("это будний день");
-            }
-            else if (a >= 6 && a <= 7)
-            {
-                Console.Write("это выходной день");
-            }
-            else
-            {
-                Console.Write("Это не день недели");
-            }
         }
     }
 }
