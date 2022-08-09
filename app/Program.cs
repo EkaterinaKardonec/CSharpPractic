302012b3cc6033c06debc2c72c4c36b3d147e5eb
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-09 21:24:49 +0700

    вводит 2 цифру из 3-значного числа

---
+++ app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите цифры ");
+
+            Console.WriteLine(Console.ReadLine()[1]);
+        }
+
+    }
+
+}
+
