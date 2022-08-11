18c4da41454a9d32f41016b66a686ec88fbb34ca
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-11 22:09:13 +0700

    проверка строки на палиндром

---
+++ app/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+            int i = 0, j = a.Length - 1;
+
+            while (i != a.Length / 2)
+            {
+                if (a[i] != a[j])
+                {
+                    Console.WriteLine("Введеное число - НЕ палиндром");
+                    Environment.Exit(0);
+                }
+
+                i++;
+                j--;
+            }
+
+            Console.WriteLine("Введеное число - палиндром");
+        }
+    }
+}
