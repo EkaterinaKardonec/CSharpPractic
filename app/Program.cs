105e2fa4c86b95e6d815fae9aa4a5fd7c627118e
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-11 15:28:20 +0700

    возведение чила в 3 степень

---
+++ app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.WriteLine("Введите число");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                        
+                    Console.WriteLine(i*i*i);
+                
+            }
+
+        }
+    }
+}
+
