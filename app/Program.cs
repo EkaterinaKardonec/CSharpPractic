b6b72d73c24e1cdec3ffd6a48328590b7a737249
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-15 21:16:10 +0700

    Возводение числа A в натуральную степень B

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,23 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите 2 числа через запятую");
+
+            string[] strVals = Console.ReadLine().Split(',');
+            int[] t = new int[strVals.Length];
+            for (int i = 0; i < strVals.Length; i++)
+            {
+                t[i] = Convert.ToInt32(strVals[i]);
+            }
+            
+            int r = 1;
+
+            for (int i = 0; i < t[1]; i++)
+            {
+                r *= t[0];
+            }
+
+            Console.WriteLine(r);
         }
     }
 }
