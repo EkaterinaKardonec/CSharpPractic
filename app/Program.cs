ef69645b53a18f0c9d0edc69fd3595860303837c
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-15 20:50:11 +0700

    Сумма цифр в числе

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,20 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            {
+
+                Console.WriteLine("Введите числа");
+                string a = Console.ReadLine();
+
+                int r = 0;
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    r = r + Convert.ToInt32(a[i].ToString());
+                }
+
+                Console.WriteLine(r);
+            }
         }
     }
 }
