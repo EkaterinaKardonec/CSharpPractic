582e2667a7294789d010db295427039add083643
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-16 21:30:24 +0700

    Ввод и вывод массива из N элементов.

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,39 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OutputArray(InputArray());
+        }
+
+        private static int[] InputArray()
+        {
+            Console.WriteLine("Введите числа через запятую ");
+
+            string[] strVals = Console.ReadLine().Split(',');
+            int[] t = new int[strVals.Length];
+
+            for (int i = 0; i < strVals.Length; i++)
+            {
+                t[i] = Convert.ToInt32(strVals[i]);
+            }
+
+            return t;
+        }
+
+        private static void OutputArray(int[] a)
+        {
+            Console.Write("[");
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]);
+
+                if (i != a.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+
+            Console.Write("]");
         }
     }
 }
