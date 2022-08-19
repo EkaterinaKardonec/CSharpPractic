d5c373af2e0c120f2205f9456739e177083fcb55
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-19 21:32:10 +0700

    Поиск разницы между max и min вещественными числами

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,46 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            double min = 0, max = 0;
+
+            Console.WriteLine("Сколько чисел вы хотите вывести?");
+
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            double[] array = new double[numberCount];
+
+            Console.Write("[");
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                array[i] = rand.NextDouble();
+
+                if (0 == i)
+                {
+                    min = array[i];
+                    max = array[i];
+                }
+
+                Console.Write(array[i]);
+
+                if (i != numberCount - 1)
+                {
+                    Console.Write(", ");
+                }
+
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            Console.WriteLine("]");
+            Console.WriteLine("Разница между max и min " + (max - min));
         }
     }
 }
