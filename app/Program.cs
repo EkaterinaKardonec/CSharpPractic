ffa037d135ad8cc22aafa4e2c73a69b1e21e2a27
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-19 18:13:40 +0700

    Сумма элементов, стоящих на нечётных позициях.

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,37 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Random rand = new Random();
+            int sum = 0;
+
+            Console.WriteLine("Сколько чисел вы хотите вывести?");
+
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numberCount];
+
+            Console.Write("[");
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                array[i] = rand.Next(100);
+                Console.Write(array[i]);
+
+                if (i != numberCount - 1)
+                {
+                    Console.Write(", ");
+                }
+
+            }
+
+            Console.WriteLine("]");
+
+            for (int k = 1; k < numberCount; k += 2)
+            {
+                sum = sum + array[k];
+            }
+            Console.WriteLine("Сумма значений - " + sum);
+
         }
     }
 }
