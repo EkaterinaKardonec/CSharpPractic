1445beeb1862f080b4b1580d5f411655557a1327
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-19 18:37:35 +0700

    подсчет количества чётных чисел в массиве

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,40 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            int honestCount = 0;
+
+            Console.WriteLine("Сколько чисел вы хотите вывести?");
+
+            int numberCount = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numberCount];
+
+            Console.Write("[");
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                array[i] = rand.Next(100, 999);
+                Console.Write(array[i]);
+
+                if (i != numberCount - 1)
+                {
+                    Console.Write(", ");
+                }
+
+            }
+
+            Console.WriteLine("]");
+
+            for (int i = 0; i < numberCount; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    honestCount++;
+                }
+
+            }
+            Console.WriteLine("Количество четных значений - " + honestCount);
+
         }
     }
 }
