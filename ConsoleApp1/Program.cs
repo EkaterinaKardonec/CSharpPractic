e62f9d9cd14f43ee282fee2a6bfccef777f2b0ae
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-22 15:21:34 +0700

    Произведение пар чисел массива

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            int j = a.Length - 1;
+
+            for (int i = 0; i <= a.Length / 2; i++)
+            {
+                if (i == j) 
+                {
+                    Console.WriteLine(a[i]);
+                    break;
+                }
+                Console.WriteLine((a[i] * a[j]));
+                j--;
+            }
+
+        }
+    }
+}
