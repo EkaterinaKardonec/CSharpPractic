771241b7babb5a6d7dc36bb997bbbf488b48b202
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-25 20:35:20 +0700

    создание двумерного массива размером m×n, заполненный случайными вещественными числами

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,18 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double[,] a = new double[3, 4];
+
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    a[i, j] = random.NextDouble();
+                    Console.Write(" " + a[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
