08e73b3ee55a9ec53693e80d9eeabb775154d1de
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-25 21:57:51 +0700

    Среднее арифметическое столбца 1вариант

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,25 @@ namespace app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double result = 0;
+            double[,] myArray = new double[,]
+            {
+              {1,4,7,2},
+              {5,9,2,3},
+              {8,4,2,4}
+            };
+
+            for (int i = 0; i < myArray.GetLength(1); i++)
+            {
+                result = 0;
+
+                for (int j = 0; j < myArray.GetLength(0); j++)
+                {
+                    result += myArray[j, i];
+                }
+
+                Console.WriteLine($"Среднее арифметическое столбца {i}: " + result / myArray.GetLength(0));
+            }
         }
     }
 }
