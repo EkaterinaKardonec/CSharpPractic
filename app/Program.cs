dd4cd6513417d79791b7acd1629c56ff76323d71
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-08-31 13:31:45 +0700

    суммf элементов в каждой строке и показывает номер строки с наименьшей суммой элементов

--- app/Program.cs
+++ app/Program.cs
@@ -6,10 +6,44 @@ namespace app
     {
         static void Main(string[] args)
         {
-            
+            int[] resultArray = { 0, 0, 0, 0 };
+            int min = 0;
+            int strNum = 0;
+            int[,] myArray = new int[,]
+                {
+                    {1,4,7,2},
+                    {5,9,2,3},
+                    {8,4,2,4},
+                    {5,2,6,7}
+                };
+            for (int i = 0; i < myArray.GetLength(0); i++)
             {
-                Console.WriteLine("Hello World!");
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    Console.Write(" " + $"{myArray[i, j]}");
+                    resultArray[i] += myArray[i, j];
+
+                }
+                Console.WriteLine();
             }
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                Console.Write(" " + resultArray[i]);
+            }
+            min = resultArray[0];
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+
+                if (resultArray[i] < min)
+                {
+                    min = resultArray[i];
+                    strNum = i;
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Номер строки с min значением "+strNum);
         }
     }
 }
+
