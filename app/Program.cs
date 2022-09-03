615ec62e0cefc972ca217b66a119f84ee212176d
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-09-03 21:20:43 +0700

    Сумма натуральных чисел от M до N

--- app/Program.cs
+++ app/Program.cs
@@ -6,10 +6,26 @@ namespace app
     {
         static void Main(string[] args)
         {
-            
+            int M, N, Sum=0;
+
+            Console.WriteLine("Введите 1 число");
+            M = Convert.ToInt32(Console.ReadLine());
+
+            if (M <= 0)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine("натуральные числа начинаются с 1");
+                return;
             }
+
+            Console.WriteLine("Введите 2 число");
+            N = Convert.ToInt32(Console.ReadLine());
+                        
+            for (int i = M; i <= N; i++)
+            {
+                Sum = i + Sum;
+            }
+
+            Console.Write(Sum);
         }
     }
 }
