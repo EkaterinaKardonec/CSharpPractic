430f408ee23c00cca6d12e377fdcedb0cedab183
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-09-03 21:40:35 +0700

    Вычисления функции Аккермана с помощью рекурсии

--- app/Program.cs
+++ app/Program.cs
@@ -6,10 +6,29 @@ namespace app
     {
         static void Main(string[] args)
         {
-            
+            int m=2, n=3;
+            Console.WriteLine(Akkerman(n,m));
+        }
+
+
+        static int Akkerman(int n, int m)
+        {
+            if (n == 0)
             {
-                Console.WriteLine("Hello World!");
+                return m + 1;
             }
+
+            if (n > 0 && m == 0)
+            {
+                return Akkerman(n - 1, 1);
+            }
+
+            if (n > 0 && m > 0)
+            {
+                return Akkerman(n - 1, Akkerman(n, m - 1));
+            }
+
+            return Akkerman(n,m);
         }
     }
 }
