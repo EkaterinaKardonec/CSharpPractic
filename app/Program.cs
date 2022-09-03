5659f43c9e4c079d3596e4039cc37875074058f9
Ekaterina Kardonec <chashinovakatrin@gmail.com>
2022-09-03 21:00:49 +0700

    Вывод натуральных чисел от M до N

--- app/Program.cs
+++ app/Program.cs
@@ -6,10 +6,33 @@ namespace app
     {
         static void Main(string[] args)
         {
-            
+            int M, N;
+
+            Console.WriteLine("Введите 1 число");
+            M = Convert.ToInt32(Console.ReadLine());
+            if (M<=0)
             {
-                Console.WriteLine("Hello World!");
+                Console.WriteLine("натуральные числа начинаются с 1");
+                return;
             }
+           
+            Console.WriteLine("Введите 2 число");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\"\"");
+
+            for (int i = M; i <= N; i++)
+            {
+                Console.Write(i);
+
+                if (i != N)
+                {
+                    Console.Write(", ");
+                }
+
+            }
+
+            Console.Write("\"\"");
         }
     }
 }
